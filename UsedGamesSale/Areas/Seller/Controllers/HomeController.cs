550d24ce2f9be77d780de387a6337eedb415606c
Matheus Peixoto <matheuspeixoto100@gmail.com>
2021-04-26 17:00:11 -0300

    Adicionando filtros para verifição de login de na área Seller no controller Home

--- UsedGamesSale/Areas/Seller/Controllers/HomeController.cs
+++ UsedGamesSale/Areas/Seller/Controllers/HomeController.cs
@@ -3,7 +3,9 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using UsedGamesSale.Models;
 using UsedGamesSale.Models.DTOs.User;
+using UsedGamesSale.Services.Filters;
 using UsedGamesSale.Services.Login;
 using UsedGamesSale.Services.UsedGamesAPI;
 
@@ -13,7 +15,7 @@ namespace UsedGamesSale.Areas.Seller.Controllers
     public class HomeController : Controller
     {
         private readonly UsedGamesAPISellers _usedGamesAPISellers;
-        private readonly SellerLoginManager _sellerLoginManager;
+        private SellerLoginManager _sellerLoginManager;
 
         public HomeController(UsedGamesAPISellers usedGamesAPISellers, SellerLoginManager sellerLoginManager)
         {
@@ -22,9 +24,35 @@ namespace UsedGamesSale.Areas.Seller.Controllers
         }
 
         [HttpGet]
+        [ValidateSellerLogin]
         public IActionResult Index()
         {
             return View();
+        }
+
+        [HttpGet]
+        public IActionResult Login()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> Login(UserLoginDTO userDTO)
+        {
+            if (!ModelState.IsValid) return View(userDTO);
+
+            UsedGamesAPIResponse response = await _usedGamesAPISellers.LoginAsync(userDTO);
+            if (!response.Success)
+            {
+                ViewData["MSG_E"] = response.Message;
+                return View(userDTO);
+            }
+
+            _sellerLoginManager.Login(response.User, response.Token);
+
+            TempData.Remove("MSG_S");
+            TempData.Add("MSG_S", "Successfully logged");
+            return RedirectToAction(nameof(Index));
         }
     }
 }
---
+++ UsedGamesSale/Services/Filters/ValidateSellerLoginAttribute.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Filters;
+using System;
+using UsedGamesSale.Services.Login;
+
+namespace UsedGamesSale.Services.Filters
+{
+    public class ValidateSellerLoginAttribute : Attribute, IAuthorizationFilter
+    {
+        public void OnAuthorization(AuthorizationFilterContext context)
+        {
+            var a = context;
+            SellerLoginManager loginManager = context.HttpContext.RequestServices.GetService(typeof(SellerLoginManager)) as SellerLoginManager;
+            if (!loginManager.IsLogged())
+                context.Result = new RedirectToActionResult("Login", "Home", new {});
+        }
+    }
+}
